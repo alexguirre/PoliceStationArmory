0459d7c725048bb6856683d51729103c60f0b987
alexguirre <alexguirre.aa@gmail.com>
2016-05-08 20:15:59 +0200

    Set AssemblyVersion to 1.3

--- PoliceStationArmoury/Properties/AssemblyInfo.cs
+++ PoliceStationArmoury/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using Rage.Attributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("56a58063-b386-4616-8dbe-02d1e74f6e46")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: Plugin("Police Station Armory", Author = "alexguirre", PrefersSingleInstance = true, SupportUrl = "http://www.lcpdfr.com/forums/topic/61717-wip-police-station-armory/")]
